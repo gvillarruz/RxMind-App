80760e1448bbed73286e84052cbbd3a0e431d480
Carlo Villarruz <carlovillarruz@Carlos-MacBook-Pro.local>
2022-01-21 18:03:20 -0500

    Add build files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,19 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './login/login.component';
-import { HomeComponent } from './home/home.component';
-import { EditComponent } from './edit/edit.component';
-import { SettingsComponent } from './settings/settings.component';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { LoginComponent } from "./login/login.component";
+import { HomeComponent } from "./home/home.component";
+import { EditComponent } from "./edit/edit.component";
+import { SettingsComponent } from "./settings/settings.component";
 
 const routes: Routes = [
-  { path: 'login', component: LoginComponent,
-children: [
-  ] },{ path: 'home', component: HomeComponent },
-  { path: 'edit', component: EditComponent },
-  { path: 'settings', component: SettingsComponent }
+  { path: "login", component: LoginComponent, children: [] },
+  { path: "home", component: HomeComponent },
+  { path: "edit", component: EditComponent },
+  { path: "settings", component: SettingsComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
