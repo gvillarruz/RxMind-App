e6b47aed6f8a6c1654c8a4da9e24a300940b7ec1
Harkiran Grewal <harkiran988@gmail.com>
2022-01-22 20:59:44 -0500

    Settings page list box
    Added a listbox to select whether to enable or disable missed dose notifications in the settings page.

--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -4,6 +4,10 @@ interface notification_locations{
   name : string;
 }
 
+interface missed_dose_notif{
+  name: string;
+}
+
 
 @Component({
   selector: 'app-settings',
@@ -16,12 +20,20 @@ export class SettingsComponent implements OnInit{
 notifilocations_list : notification_locations[];
 selectednotiflocation!: notification_locations;
 
+missed_dose_option : missed_dose_notif[];
+selectedmissed_dose_option !: missed_dose_notif;
+
+
   constructor() { 
 
     this.notifilocations_list = [
       { name: "Dispensing Device"},
       {name: "Email"},
       {name: "Text"} ];
+
+    this.missed_dose_option = [
+      {name: "Enable"},
+      {name: "Disable"}];
 
    }
 
