e132abda835225aae8b5d9b4c6b7e56eb0531db8
Harkiran Grewal <harkiran988@gmail.com>
2022-01-22 20:54:22 -0500

    Settings Page notification selection
    Added implementation for the listbox to select the location of notifications on the settings page.

--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 
+interface notification_locations{
+  name : string;
+}
+
+
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.component.html',
   styleUrls: ['./settings.component.scss']
 })
-export class SettingsComponent implements OnInit {
 
-  constructor() { }
+export class SettingsComponent implements OnInit{
+
+notifilocations_list : notification_locations[];
+selectednotiflocation!: notification_locations;
+
+  constructor() { 
+
+    this.notifilocations_list = [
+      { name: "Dispensing Device"},
+      {name: "Email"},
+      {name: "Text"} ];
+
+   }
 
   ngOnInit(): void {
   }
 
 }
+
