7cc9a4943babaa5709adee7798c104c848b78929
Harkiran Grewal <harkiran988@gmail.com>
2022-01-22 19:57:00 -0500

    Update imports in app.module.ts
    Added listbox item import to the appmodule.ts file.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { FormsModule }    from '@angular/forms';
 import {ButtonModule} from 'primeng/button';
 import { RippleModule } from 'primeng/ripple';
 import {SliderModule} from 'primeng/slider';
+import {ListboxModule} from 'primeng/listbox';
 
 @NgModule({
   declarations: [
@@ -32,7 +33,8 @@ import {SliderModule} from 'primeng/slider';
     FormsModule,
     ButtonModule,
     RippleModule,
-    SliderModule
+    SliderModule, 
+    ListboxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
