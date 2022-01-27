446fc982e6ee33ce9cbfc969663b0f14ea14ced0
Carlo Villarruz <gvillarruz@ryerson.ca>
2022-01-26 19:07:12 -0500

    Restructuring

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,22 +4,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { SettingsComponent } from './settings/settings.component';
+import { SettingsComponent } from './home/settings/settings.component';
 import { HomeComponent } from './home/home.component';
-import { EditComponent } from './edit/edit.component';
+import { EditComponent } from './home/edit/edit.component';
 import {InputTextModule} from 'primeng/inputtext';
 import {PasswordModule} from 'primeng/password';
 import { CommonModule } from '@angular/common';
 import { FormsModule }    from '@angular/forms';
 import {ButtonModule} from 'primeng/button';
 import { RippleModule } from 'primeng/ripple';
+import { OverviewComponent } from './home/overview/overview.component';
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     SettingsComponent,
     HomeComponent,
-    EditComponent
+    EditComponent,
+    OverviewComponent
   ],
   imports: [
     BrowserModule,
