5f8e8a2ee88c338e39eb478df5595e8e12736648
Carlo Villarruz <gvillarruz@ryerson.ca>
2022-02-06 18:41:27 -0500

    Fix routing

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,23 +1,22 @@
-import { Component, OnInit } from '@angular/core';
-import {MenuItem} from 'primeng/api';
-import { Menubar } from 'primeng/menubar';
-
+import { Component, OnInit } from "@angular/core";
+import { MenuItem } from "primeng/api";
+import { Menubar } from "primeng/menubar";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.scss"],
 })
 export class HomeComponent implements OnInit {
-  menuitems : MenuItem[];
+  menuitems: MenuItem[];
 
   constructor() {
     this.menuitems = [
-      {label: 'Home', routerLink: ['/overview']},
-      {label: 'Add or Edit Medications', routerLink:['/edit']},
-      {label: "Settings", routerLink:['/settings']}];
+      { label: "Home", routerLink: "overview" },
+      { label: "Add or Edit Medications", routerLink: "edit" },
+      { label: "Settings", routerLink: "settings" },
+    ];
   }
-  
-  ngOnInit():void{}
 
+  ngOnInit(): void {}
 }
