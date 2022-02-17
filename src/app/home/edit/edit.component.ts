1720fff19c50a3da49cb5b70d6531aea4a3ea544
Harkiran Grewal <harkiran988@gmail.com>
2022-02-16 20:45:24 -0500

    Post and subscribe edit page
    Added post and subscribe calls to the edit page buttons.

--- src/app/home/edit/edit.component.ts
+++ src/app/home/edit/edit.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
 
 //Frequency of Dosage
 interface dose_frequency{
@@ -34,24 +35,7 @@ export class EditComponent implements OnInit {
   medication_list : medication[];
   selectedmedication? :medication;
 
-  onSelect(medication: medication) : void{
-    this.selectedmedication = medication;
-    //the user selects a medication
-    //load the associated medication metadata into the form fields
-  }
-
-  saveMedication(){
-    //take the data in all of the form fields, package it into an object
-    //send the data to save in the web server 
-    //wait for a success response from the web server & display success to user
-  }
-
-  removeMedication(){
-    //Take the medication data in all of the form fields, package it into an object
-    //Send the data to the web server to remove 
-  }
-
-  constructor() { 
+  constructor(private http: HttpClient) { 
     this.dosefreq_list = [
       {frequency: "Once a Day"},
       {frequency: "Twice a Day"},
@@ -70,6 +54,49 @@ export class EditComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    console.log("Initialized the add or edit page");
+  }
+
+  onSelect(medication: medication) : void{
+    this.selectedmedication = medication;
+    //the user selects a medication
+    //load the associated medication metadata into the form fields
+  }
+
+  saveMedication(){
+    //take the data in all of the form fields, package it into an object
+    //send the data to save in the web server 
+    //wait for a success response from the web server & display success to user
+    this.http
+    .post("https://www.rxmind.tech/crud", {
+      type: "add",
+      payload: {
+        name: "Levothyroxine",
+        timesDaily: 2,
+        dispenseTimes: ["12:00PM", "4:00PM"],
+        pillsAdded: 50,
+        pillsPerDose: 1
+    }})
+    .subscribe((data) => {
+      console.log(data);
+    });
+
+    //Differentiate between adding a new medication and saving a current one 
+    console.log("End of the saveMedication btn function");
+  }
+
+  removeMedication(){
+    //Take the medication data in all of the form fields, package it into an object
+    //Send the data to the web server to remove
+    this.http
+    .post("https://www.rxmind.tech/crud", {
+      type: "delete",
+      payload: {
+        name: "Levothyroxine"
+    }})
+    .subscribe((data) => {
+      console.log(data);
+    });
   }
 
 }
