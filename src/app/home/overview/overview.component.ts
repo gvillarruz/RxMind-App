cf734c7d736f96eb25f6cd1f2952fec9dc91a1f2
Harkiran Grewal <harkiran988@gmail.com>
2022-03-13 17:00:24 -0400

    Overview page warnings
    Added html elements and triggers to update with medication warnings on initialization of the home page.

--- src/app/home/overview/overview.component.ts
+++ src/app/home/overview/overview.component.ts
@@ -8,6 +8,7 @@ interface medication{
 var Medication1_info: any[];
 var Medication2_info: any[];
 var Medication3_info: any[];
+var MedWarnings: any[];
 
 @Component({
   selector: 'app-overview',
@@ -17,11 +18,14 @@ var Medication3_info: any[];
 export class OverviewComponent implements OnInit {
   calendarvalue !: Date;
   sliderval : number= 70;
-  medleft : string = "2 Weeks";
-  medprogressnum : number = 70;
+  medleft :string = "";
+  medprogressnum : number = 0;
 
   medication_list : medication[];
   selectedmedication? :medication;
+
+  warnings_list !:any[];
+  dismisswarningvisible = false;
 
   constructor(private http: HttpClient) {
     this.medication_list = [
@@ -32,6 +36,7 @@ export class OverviewComponent implements OnInit {
       Medication1_info = [];
       Medication2_info = [];
       Medication3_info = [];
+      MedWarnings = [];
    }
 
   ngOnInit(): void {
@@ -57,36 +62,55 @@ export class OverviewComponent implements OnInit {
             Medication2_info[1] = data.medications[1].remainingPills;
             
             if(nmeds == 3){
-              Medication3_info[0] = data.medication[2].name;
-              this.medication_list[2].name = data.medications[2].name;
-              //Gathering remaining pills info
-              Medication3_info[1] = data.medications[2].remainingPills;
+             Medication3_info[0] = data.medication[2].name;
+             this.medication_list[2].name = data.medications[2].name;
+             //Gathering remaining pills info
+             Medication3_info[1] = data.medications[2].remainingPills;
             }
           }
         }
 
         //Gathering any warning info from the server 
-        /*if(data.warning.length != 0){
+        if(data.warnings.length != 0){
+          for(let i=0; i< data.warnings.length; i++){
+            MedWarnings[i] = data.warning[i]; }
+            this.dismisswarningvisible = true;
+        }
 
-        }*/
         console.log(data);
       });
   }
 
   onSelect(medication: medication) : void{
     this.selectedmedication = medication;
-    //Update the amount of meds left progress bar 
 
-    //Call the calendar command here
+    //Figure out which medicationx_info array the selected medication is from
+    //Update the number of medications left and the progress bar in the UI
+    if(this.selectedmedication.name == Medication1_info[0]){
+      this.medleft = Medication1_info[1];
+      this.medprogressnum = Medication1_info[1]; }
 
+    else if(this.selectedmedication.name == Medication2_info[0]){
+      this.medleft = Medication2_info[1];
+      this.medprogressnum = Medication2_info[1]; }
 
-    //Populate the data in the view for the selected medication
+    else if(this.selectedmedication.name == Medication3_info[0]){
+      this.medleft = Medication3_info[1];
+      this.medprogressnum = Medication3_info[1]; }
+  }
+
+  onSelectDate(calendarvalue: Date){
+    //Make a post request with the date 
+
+    //Capture the return
+    //Based on the selected medication, output the medication information
   }
 
   Dismiss_notif(){
     //Dismiss the missed notification when the user clicks this button 
     //Clear the notification label
     //Clear the notification in the web-server
+    this.dismisswarningvisible = false;
   }
 
 }
