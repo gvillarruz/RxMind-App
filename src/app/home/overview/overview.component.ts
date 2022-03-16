236c5b3e90ef7ea0b7c7ceee53dcbb2bd1c8aa04
Harkiran Grewal <harkiran988@gmail.com>
2022-03-16 18:40:04 -0400

    Added medication interface definition
    Added the medication interface definition so the onSelect() and calendar select method operates correctly.

--- src/app/home/overview/overview.component.ts
+++ src/app/home/overview/overview.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { HomeService } from "../../home.service";
 
+interface medication{
+  name: String;
+}
+
 @Component({
   selector: "app-overview",
   templateUrl: "./overview.component.html",
@@ -19,7 +23,7 @@ export class OverviewComponent implements OnInit {
   Medication3_info = [];
   MedWarnings = [];
 
-  selectedmedication?: { name: string };
+  selectedmedication: medication;
 
   warnings_list!: any[];
   dismisswarningvisible = false;
@@ -71,7 +75,7 @@ export class OverviewComponent implements OnInit {
       });
   }
 
-  onSelect(medication): void {
+  onSelect(medication: medication): void {
     this.selectedmedication = medication;
 
     //Figure out which medicationx_info array the selected medication is from
@@ -90,10 +94,11 @@ export class OverviewComponent implements OnInit {
 
   onSelectDate(calendarvalue: Date) {
     //Make a post request with the date, first format the date
+    let date_send = "date"
     let dd = String(calendarvalue.getDate()).padStart(2, "0");
     let mm = String(calendarvalue.getMonth() + 1).padStart(2, "0");
     let yyyy = calendarvalue.getFullYear();
-    let date_send = yyyy + "-" + mm + "-" + dd;
+    date_send = yyyy + "-" + mm + "-" + dd;
 
     console.log("Inside the onSelectDate function with date:", date_send);
     this.http
@@ -103,22 +108,25 @@ export class OverviewComponent implements OnInit {
       .subscribe((data: any) => {
         console.log(data);
         //Capture the return
-        //Based on the selected medication, output the medication information for that day
-        if (this.selectedmedication == this.Medication1_info[0]) {
+        //Based on the number of medications that are returned, output the medication information for that day 
+        //let num_meds = data.medications.length;
+        if (this.selectedmedication.name == this.Medication1_info[0]) {
           this.calendarmedtaken = data.medications[0].medicationTaken;
           this.calendartimetaken = data.medications[0].timeTaken;
           this.Iscalendarmedtakenvisible = true;
           this.Iscalendartimetakenvisible = true;
-        } else if (this.selectedmedication == this.Medication2_info[0]) {
-          this.calendarmedtaken = data.medications[1].medicationTaken;
-          this.calendartimetaken = data.medications[1].timeTaken;
-          this.Iscalendarmedtakenvisible = true;
-          this.Iscalendartimetakenvisible = true;
-        } else if (this.selectedmedication == this.Medication3_info[0]) {
-          this.calendarmedtaken = data.medications[2].medicationTaken;
-          this.calendartimetaken = data.medications[2].timeTaken;
-          this.Iscalendarmedtakenvisible = true;
-          this.Iscalendartimetakenvisible = true;
+          if (this.selectedmedication.name == this.Medication2_info[0]) {
+            this.calendarmedtaken = data.medications[1].medicationTaken;
+            this.calendartimetaken = data.medications[1].timeTaken;
+            this.Iscalendarmedtakenvisible = true;
+            this.Iscalendartimetakenvisible = true;
+            if (this.selectedmedication.name == this.Medication3_info[0]) {
+              this.calendarmedtaken = data.medications[2].medicationTaken;
+              this.calendartimetaken = data.medications[2].timeTaken;
+              this.Iscalendarmedtakenvisible = true;
+              this.Iscalendartimetakenvisible = true;
+            }    
+          }
         }
       });
   }
